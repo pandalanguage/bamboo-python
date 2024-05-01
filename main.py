4f567f4ce3445fa095e241ebb487ea1a88658dab
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-05-01 14:57:09 +0800

    新增 获取语言。

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 code = input("请输入您的代码文件名：")
 with open(code, 'r') as f:
-    data = f.readlines()[1]
+    data = f.readlines()[0]
     language = data.replace('#语言 = ', "")
+    language = language.replace('\"', "")
     print(language)
